02df31b35faf45d51b2d603f02b3d00fcf8a3832
chaim cymerman <a548492309@gmail.com>
2023-08-14 15:36:16 +0300

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import _ from 'lodash';
 export function readTextFile(filename){
     fs.readFile(filename,'utf-8',(err, data) => {
         if (err) throw new Error('An error occurred: ' + err.message);
-
         console.log(data);
     })
 }
@@ -62,13 +61,13 @@ export function uniqItemsTextFileAper(filename) {
     fs.readFile(filename, 'utf-8', (err, data) => {
         console.error('An error occurred:', err);
 
-        let wordArray = _.words(data) + '/n';
+        let wordArray = _.words(data);
         let uniqueWords = _.uniq(wordArray);
         let uniqueWordsFilter = uniqueWords.filter(word => word.length>5);
         console.log(uniqueWordsFilter)
         let upperCaseWords = uniqueWordsFilter.map(word => word.toUpperCase());
         console.log(upperCaseWords)
-        fs.appendFile('text111.txt', String(upperCaseWords), (err) => {
+        fs.appendFile('text1.txt', String(upperCaseWords) + '\n', (err) => {
             if (err) throw new Error('An error occurred: ');
             console.log('The file has been saved!');
         });
---
+++ app2.js
@@ -0,0 +1,5 @@
+
+import fs from 'fs';
+
+import { readTextFile, contTextFile, reversTextFile, uniqItemsTextFile, uniqItemsTextFileAmount, uniqItemsTextFileAper } from './app.js';
+readTextFile('./randomText.txt')
